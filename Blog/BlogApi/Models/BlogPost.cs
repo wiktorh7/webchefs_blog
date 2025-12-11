6b108ba6ba13508d8d3e2b932cb8a5143bc7c4c3
wojciechm0 <wojciech.jakub.majchrzyk@gmail.com>
2025-12-11 17:19:41 +0100

    Http request i nullable

--- Blog/BlogApi/Models/BlogPost.cs
+++ Blog/BlogApi/Models/BlogPost.cs
@@ -4,26 +4,26 @@ namespace BlogApi.Models
 {
     public class BlogPost
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Required]
-        public string author { get; set; }
+        public string? author { get; set; }
 
         [Required]
-        public string position { get; set; }
+        public string? position { get; set; }
 
         [Required]
-        public string bio { get; set; }
+        public string? bio { get; set; }
 
         [Required]
-        public DateTime date { get; set; } = DateTime.UtcNow;
+        public DateTime? date { get; set; } = DateTime.UtcNow;
 
-        public string img_url { get; set; }
-
-        [Required]
-        public string header{ get; set; }
+        public string? img_url { get; set; }
 
         [Required]
-        public string content{ get; set; }
+        public string? header{ get; set; }
+
+        [Required]
+        public string? content{ get; set; }
     }
 }
--- Blog/BlogApi/Program.cs
+++ Blog/BlogApi/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using MySqlX.XDevAPI.Common;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<BlogDbContext>(opt => opt.UseSqlite("Blog"));
+builder.Services.AddDbContext<BlogDbContext>(opt => opt.UseSqlite("Data Source=Blog.db"));
 
 var app = builder.Build();
 
