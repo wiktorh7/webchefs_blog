9c35f5440f6273a34250189a639158df47bfd0bb
wojciechm0 <wojciech.jakub.majchrzyk@gmail.com>
2025-12-12 10:45:36 +0100

    Update post details in BlogApi.http

--- Blog/BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ Blog/BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb0de246b2781ac5c5f21e84ba6645e3e9efad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b5ed595d038172cf5b0aeae70cacb9fde6156e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
