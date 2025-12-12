d8b5ed595d038172cf5b0aeae70cacb9fde6156e
wojciechm0 <wojciech.jakub.majchrzyk@gmail.com>
2025-12-12 10:37:23 +0100

    Enhance BlogPost model validation and update API endpoints for post management

--- Blog/BlogApi/Models/BlogPost.cs
+++ Blog/BlogApi/Models/BlogPost.cs
@@ -7,23 +7,29 @@ namespace BlogApi.Models
         public int Id { get; set; }
 
         [Required]
+        [MinLength(5)]
         public string author { get; set; } = string.Empty;
  
         [Required]
+        [MinLength(5)]
         public string position { get; set; } = string.Empty;
 
         [Required]
+        [MinLength(5)]
         public string bio { get; set; } = string.Empty;
 
         [Required]
         public DateTime date { get; set; } = DateTime.UtcNow;
 
+        [MinLength(10)]
         public string img_url { get; set; } = string.Empty;
 
         [Required]
+        [MinLength(10)]
         public string header{ get; set; } = string.Empty;
 
         [Required]
+        [MinLength(50)]
         public string content{ get; set; } = string.Empty;
     }
 }
